0c9c8c5510d1684d5f88f703967da401d3c0ae95
Victorcsil <victorcastrosdev@gmail.com>
2025-01-27 19:56:06 -0300

    Finished configurations

--- orchestrate/airflow/dags/stage2.py
+++
@@ -1,9 +0,0 @@
-from airflow import DAG
-from airflow.operators.bash import BashOperator
-from datetime import datetime
-
-with DAG("load", start_date=datetime(2021,1,1), catchup=False, schedule_interval='@daily') as dag:
-    task1 = BashOperator(
-        task_id="task1",
-        bash_command="cd /home/victor/Desktop/TESTE/code-challenge; .meltano/run/bin run tap-csv target-postgres"
-    )
